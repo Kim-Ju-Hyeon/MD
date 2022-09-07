22a485e1927c314b0defab1de5f8530c7e12c495
KimJuHyeon <wngus1310@naver.com>
2022-09-08 01:40:52 +0900

    inference

--- run_inference.py
+++ run_inference.py
@@ -3,6 +3,7 @@ from runner.runner import Runner
 import traceback
 from utils.logger import setup_logging
 import os
+from glob import glob
 from utils.train_helper import set_seed, mkdir, edict2dict
 import datetime
 
@@ -14,19 +15,21 @@ import yaml
 @click.command()
 @click.option('--conf_file_path', type=click.STRING, default=None)
 def main(conf_file_path):
-    config = edict(yaml.load(open(conf_file_path, 'r'), Loader=yaml.FullLoader))
+    dirs = glob(os.path.join(conf_file_path, '*/config.yaml'))
+    for config_file in dirs:
+        config = edict(yaml.load(open(config_file, 'r'), Loader=yaml.FullLoader))
 
-    log_file = os.path.join(config.exp_sub_dir, "log_inference_{}.txt".format(config.seed))
-    logger = setup_logging('INFO', log_file, logger_name=str(config.seed))
-    logger.info("Writing log file to {}".format(log_file))
-    logger.info("Exp instance id = {}".format(config.exp_name))
+        log_file = os.path.join(config.exp_sub_dir, "log_inference_{}.txt".format(config.seed))
+        logger = setup_logging('INFO', log_file, logger_name=str(config.seed))
+        logger.info("Writing log file to {}".format(log_file))
+        logger.info("Exp instance id = {}".format(config.exp_name))
 
-    try:
-        my_runner = Runner(config=config)
-        my_runner.test()
+        try:
+            my_runner = Runner(config=config)
+            my_runner.test()
 
-    except:
-        logger.error(traceback.format_exc())
+        except:
+            logger.error(traceback.format_exc())
 
 
 if __name__ == '__main__':
